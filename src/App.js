68e850e33744920b4ce3ff4e65534133f39a7c02
Anton Jaldegren <anton.jaldegren@gmail.com>
2022-08-15 11:59:49 +0200

    Changed api url

--- src/App.js
+++ src/App.js
@@ -8,141 +8,140 @@ import ReactCanvasConfetti from "react-canvas-confetti";
 import MatchedList from "./components/MatchedList";
 
 function App() {
-  const [list, setList] = useState([]);
-  const [matchedWith, setMatchedWith] = useState([]);
-  const [matchmakeCount, setMatchmakeCount] = useState(0);
-  const socket = useRef();
-  // Timestamp: Indikation på hur längesen någon bad om hjälp
-  // ✅ Math.random för random färg
-  // Kevins MP3-ljud
-  // ✅ Namn, Breakout room (optional)
-  // Ändra favicon/title om listan är tom eller inte
-  // Wishlist: Notifikation
+	const [list, setList] = useState([]);
+	const [matchedWith, setMatchedWith] = useState([]);
+	const [matchmakeCount, setMatchmakeCount] = useState(0);
+	const socket = useRef();
+	// Timestamp: Indikation på hur längesen någon bad om hjälp
+	// ✅ Math.random för random färg
+	// Kevins MP3-ljud
+	// ✅ Namn, Breakout room (optional)
+	// Ändra favicon/title om listan är tom eller inte
+	// Wishlist: Notifikation
 
-  useEffect(() => {
-    socket.current = io("localhost:8000");
+	useEffect(() => {
+		socket.current = io("https://waiting-list-backend.herokuapp.com/");
 
-    socket.current.on("connect", () => {
-      console.log("Connected");
-    });
+		socket.current.on("connect", () => {
+			console.log("Connected");
+		});
 
-    socket.current.on("new", (data) => {
-      console.log(data);
-      setList(data);
-    });
+		socket.current.on("new", (data) => {
+			console.log(data);
+			setList(data);
+		});
 
-    socket.current.on("error", (err) => {
-      console.log(err);
-    });
+		socket.current.on("error", (err) => {
+			console.log(err);
+		});
 
-    socket.current.on("confetti", () => {
-      fire();
-    });
+		socket.current.on("confetti", () => {
+			fire();
+		});
 
-    socket.current.on("matchmake:notify", (length) => {
-      if (length === 0) {
-        setMatchedWith([])
-      }
-      setMatchmakeCount(length);
+		socket.current.on("matchmake:notify", (length) => {
+			if (length === 0) {
+				setMatchedWith([]);
+			}
+			setMatchmakeCount(length);
+		});
 
-    });
+		socket.current.on("matchmake:update", (list) => {
+			setMatchedWith(list);
+		});
 
-    socket.current.on("matchmake:update", (list) => {
-      setMatchedWith(list);
-    });
+		socket.current.onAny((event, ...args) => {
+			console.log(event, args);
+		});
+	}, []);
 
-    socket.current.onAny((event, ...args) => {
-      console.log(event, args);
-    });
-  }, []);
+	function raiseHand(name, room) {
+		socket.current.emit("help", {
+			name: name,
+			room: room,
+			color: getRandomColor(),
+		});
+	}
 
-  function raiseHand(name, room) {
-    socket.current.emit("help", {
-      name: name,
-      room: room,
-      color: getRandomColor(),
-    });
-  }
+	function lowerHand(id) {
+		console.log("Removed: " + id);
+		socket.current.emit("done", {
+			id: id,
+		});
+	}
 
-  function lowerHand(id) {
-    console.log("Removed: " + id);
-    socket.current.emit("done", {
-      id: id,
-    });
-  }
+	function joinMatchmaking(name) {
+		if (name) {
+			socket.current.emit("matchmake:join", { name });
+		}
+	}
 
-  function joinMatchmaking(name) {
-    if (name) {
-      socket.current.emit("matchmake:join", { name });
-    }
-  }
+	const animationInstanceRef = useRef(null);
 
-  const animationInstanceRef = useRef(null);
+	const getInstance = useCallback((instance) => {
+		animationInstanceRef.current = instance;
+	}, []);
 
-  const getInstance = useCallback((instance) => {
-    animationInstanceRef.current = instance;
-  }, []);
+	const makeShot = useCallback((ratio, opts) => {
+		animationInstanceRef.current &&
+			animationInstanceRef.current({
+				...opts,
+				origin: { y: 0.7 },
+				particleCount: Math.floor(200 * ratio),
+			});
+	}, []);
 
-  const makeShot = useCallback((ratio, opts) => {
-    animationInstanceRef.current &&
-      animationInstanceRef.current({
-        ...opts,
-        origin: { y: 0.7 },
-        particleCount: Math.floor(200 * ratio),
-      });
-  }, []);
+	const fire = useCallback(() => {
+		makeShot(0.25, {
+			spread: 26,
+			startVelocity: 55,
+		});
 
-  const fire = useCallback(() => {
-    makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    });
+		makeShot(0.2, {
+			spread: 60,
+		});
 
-    makeShot(0.2, {
-      spread: 60,
-    });
+		makeShot(0.35, {
+			spread: 100,
+			decay: 0.91,
+			scalar: 0.8,
+		});
 
-    makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    });
+		makeShot(0.1, {
+			spread: 120,
+			startVelocity: 25,
+			decay: 0.92,
+			scalar: 1.2,
+		});
 
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    });
+		makeShot(0.1, {
+			spread: 120,
+			startVelocity: 45,
+		});
+	}, [makeShot]);
 
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    });
-  }, [makeShot]);
-
-  return (
-    <div>
-      <ReactCanvasConfetti
-        refConfetti={getInstance}
-        style={{
-          position: "fixed",
-          pointerEvents: "none",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-        }}
-      />
-      <InputBox
-        onRaiseHand={raiseHand}
-        onJoinMatchmaking={joinMatchmaking}
-        matchmakeCount={matchmakeCount}
-      />
-      <MatchedList matchedWith={matchedWith} socket={socket.current} />
-      <WaitingList onLowerHand={lowerHand} list={list} />
-    </div>
-  );
+	return (
+		<div>
+			<ReactCanvasConfetti
+				refConfetti={getInstance}
+				style={{
+					position: "fixed",
+					pointerEvents: "none",
+					width: "100%",
+					height: "100%",
+					top: 0,
+					left: 0,
+				}}
+			/>
+			<InputBox
+				onRaiseHand={raiseHand}
+				onJoinMatchmaking={joinMatchmaking}
+				matchmakeCount={matchmakeCount}
+			/>
+			<MatchedList matchedWith={matchedWith} socket={socket.current} />
+			<WaitingList onLowerHand={lowerHand} list={list} />
+		</div>
+	);
 }
 
 export default App;
